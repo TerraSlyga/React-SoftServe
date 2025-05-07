496b55be0d5ab81be01532f3e62f81337baa2978
Misha <mishasamonov@gmail.com>
2025-05-07 19:36:08 +0300

    Add BuyTicketButton

---
+++ src/components/BuyTicketButton/BuyTicketButton.jsx
@@ -0,0 +1,11 @@
+import "./BuyTicketButton.css"
+
+const BuyTicketButton = () => {
+  return (
+    <div className="buy-ticket-container">
+      <button className="buy-ticket-button">КУПИТИ</button>
+    </div>
+  )
+}
+
+export default BuyTicketButton
