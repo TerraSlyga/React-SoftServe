b06f753e5bdcd5419abb418f5273d0132af8d5da
Olexander Krupskiy <terraslyga2004@gmail.com>
2025-05-09 22:02:57 +0300

    add SessionsAdminPage mock

--- src/pages/admin/SessionsAdminPage/SessionsAdminPage.jsx
+++ src/pages/admin/SessionsAdminPage/SessionsAdminPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSessions, deleteSession } from "../../store/sessionsSlice.js";
+import { fetchSessions, deleteSession } from "../../../store/sessionsSlice.js";
 import { Link } from "react-router-dom";
 
 export default function SessionsAdminPage() {
