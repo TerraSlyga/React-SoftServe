15b99f43fbc1f4c252e68df13a0e48a742ef17b4
dimamedvid <dima.medvid.06@gmail.com>
2025-05-12 15:12:51 +0300

    Fixed footer

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,15 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import "./MainLayout.css";
 
 export default function MainLayout() {
   return (
-    <div className="layout">
-      <Header />
-      <div className="layout__content">
-        <Outlet />
+      <div className="layout">
+        <Header />
+        <div className="layout__content">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+
   );
 }
